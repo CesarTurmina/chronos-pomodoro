fe6f392711f295d3f23b2caeec687cefe201f045
Cesar Turmina <cesarturmina@gmail.com>
2025-07-05 09:22:47 -0300

    Criação de Componentes da Logo e Estilização inicial.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
 
 export function App() {
-  console.log('Oi');
-
   return (
     <>
-      <Heading attr={123} attr2='String'>
-        Olá mundo 1
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nisi
-        possimus rem reprehenderit a officiis odio pariatur asperiores. Ad
-        veniam doloremque dicta, dolorem dolores ex ab ea id quaerat, enim
-        dolorum laboriosam, itaque voluptas vel adipisci consequuntur? Ipsum
-        tempore beatae laudantium modi qui illo, animi similique voluptas
-        dolorum, aperiam quos!
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
