670b0a1e9c8fb2e9f9549b7331cf5afd100fe244
Cesar Turmina <cesarturmina@gmail.com>
2025-07-05 07:15:39 -0300

    Ajustes de conhecimento props

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ export function App() {
 
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2='String'>
+        Olá mundo 1
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nisi
         possimus rem reprehenderit a officiis odio pariatur asperiores. Ad
